43aaee74ea8b43f77a2a6733b77bd4d5b003778f
huideyeren <takakura.yusuke@gmail.com>
2020-05-30 20:19:35 +0900

    Supports switching of processing by arguments.

--- darkroom.py
+++ darkroom.py
@@ -1,8 +1,26 @@
+import argparse
 import datetime
 import glob
 from skimage.color import rgb2gray
 from skimage import io, img_as_float, img_as_ubyte, util, exposure
 import warnings
+
+parser = argparse.ArgumentParser(description='Negative Film Utility')
+
+parser.add_argument(
+    '-m',
+    '--monochrome',
+    help='Convert to monochrome.',
+    action='store_true'
+    )
+parser.add_argument(
+    '-n',
+    '--negative',
+    help='Convert negative to positive.',
+    action='store_true'
+    )
+
+args = parser.parse_args()
 
 now = datetime.datetime.now()
 now_s = now.strftime('%Y-%m-%d_%H:%M:%S')
@@ -12,15 +30,21 @@ files = glob.glob('./negative/*.jpg')
 for i, file in enumerate(files):
     img = io.imread(file)
     img = img_as_float(img)
-    img_inverted = util.invert(img)
+
+    if args.negative is True:
+        img = util.invert(img)
     
-    imgL = exposure.adjust_gamma(img_inverted, 2.2)
-    img_grayL = rgb2gray(imgL)
-    img_gray = exposure.adjust_gamma(img_grayL, 1.0/2.2)
+    if args.monochrome is True:
+        imgL = exposure.adjust_gamma(img, 2.2)
+        img_grayL = rgb2gray(imgL)
+        img = exposure.adjust_gamma(img_grayL, 1.0/2.2)
+        
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
-        img_gray = img_as_ubyte(img_gray)
-    io.imsave(('./positive/{0}_{1}.jpg'.format(now_s, (i + 1))), img_gray, quality=100)
+        img = img_as_ubyte(img)
+
+    io.imsave(('./positive/{0}_{1}.jpg'.format(now_s, (i + 1))), img, quality=100)
     print('{0}/{1} done!'.format((i + 1), str(len(files))))
 
 print('All images have been converted!')
+
